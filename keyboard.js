178c575f2d86387629512e6923e45e5ccf31fdfd
ZeBeatWaves <hitechfive@gmail.com>
2022-07-18 16:54:44 +0300

    start version free keyboard

---
+++ keyboard.js
@@ -0,0 +1,190 @@
+var svgns = "http://www.w3.org/2000/svg";
+var octaveKeyboardGlobal = 4;
+
+function createKeyWhite(position, id)
+{
+    var rect = document.createElementNS(svgns, 'rect');
+    rect.setAttribute('x', position);
+    rect.setAttribute('y', '0');
+    rect.setAttribute('height', '110');
+    rect.setAttribute('width', '20');
+    rect.setAttribute('rx', '5');
+    rect.setAttribute('ry', '5');
+    rect.setAttribute('class', 'keysWhite');
+    rect.setAttribute('id', id);
+    document.getElementById('keyboard').appendChild(rect);
+}
+
+function createKeyBlack(position, id)
+{
+    var rect = document.createElementNS(svgns, 'rect');
+    rect.setAttribute('x', position);
+    rect.setAttribute('y', '0');
+    rect.setAttribute('height', '65');
+    rect.setAttribute('width', '15');
+    rect.setAttribute('fill', '#000000');
+    rect.setAttribute('stroke', '#000000');
+    rect.setAttribute('rx', '5');
+    rect.setAttribute('ry', '5');
+    rect.setAttribute('class', 'keysBlack');
+    rect.setAttribute('id', id);
+    document.getElementById('keyboard').appendChild(rect);
+}
+
+function createKeyboard(){
+    document.getElementById('keyboard').setAttribute('width', '1050px');
+    for (let index = 0; index < 88; index++) {
+        var noteName = keysListGlobal[index].nameNotes;
+        if (!noteName.includes('#')) {
+            createKeyWhite(keysListGlobal[index].position, keysListGlobal[index].nameNotes);
+        }
+    }
+    for (let index = 0; index < 88; index++) {
+        var noteName = keysListGlobal[index].nameNotes;
+        if (noteName.includes('#')) {
+            createKeyBlack(keysListGlobal[index].position, keysListGlobal[index].nameNotes);
+        }
+    }
+}
+
+function activeKeys(notesArray){
+    if (Array.isArray(notesArray) == true) {
+        for (let index = 0 ; index < notesArray.length; index++) {
+            var key = document.getElementById(notesArray[index]);
+            key.style.fill = '#d9114a';
+        }
+    }
+    else{
+        let notes = notesArray.split(',');
+        for (let index = 0; index <  notes.length; index++) {
+            var key = document.getElementById(notes[index]);
+            key.style.fill = '#d9114a';
+        }
+    }
+}
+
+document.addEventListener('keydown', function(event) {
+    switch (event.code) {
+        case "KeyA":
+            var key = document.getElementById('C' + octaveKeyboardGlobal);
+            key.style.fill = '#d9114a';
+            break;
+        case "KeyW":
+            var key = document.getElementById('C#' + octaveKeyboardGlobal);
+            key.style.fill = '#d9114a';
+            break;
+        case "KeyS":
+            var key = document.getElementById('D' + octaveKeyboardGlobal);
+            key.style.fill = '#d9114a';
+            break;
+        case "KeyE":
+            var key = document.getElementById('D#' + octaveKeyboardGlobal);
+            key.style.fill = '#d9114a';
+            break;
+        case "KeyD":
+            var key = document.getElementById('E' + octaveKeyboardGlobal);
+            key.style.fill = '#d9114a';
+            break;
+        case "KeyF":
+            var key = document.getElementById('F' + octaveKeyboardGlobal);
+            key.style.fill = '#d9114a';
+            break;
+        case "KeyT":
+            var key = document.getElementById('F#' + octaveKeyboardGlobal);
+            key.style.fill = '#d9114a';
+            break;
+        case "KeyG":
+            var key = document.getElementById('G' + octaveKeyboardGlobal);
+            key.style.fill = '#d9114a';
+            break;
+        case "KeyY":
+            var key = document.getElementById('G#' + octaveKeyboardGlobal);
+            key.style.fill = '#d9114a';
+            break;
+        case "KeyH":
+            var key = document.getElementById('A' + octaveKeyboardGlobal);
+            key.style.fill = '#d9114a';
+            break;
+        case "KeyU":
+            var key = document.getElementById('A#' + octaveKeyboardGlobal);
+            key.style.fill = '#d9114a';
+            break;
+        case "KeyJ":
+            var key = document.getElementById('B' + octaveKeyboardGlobal);
+            key.style.fill = '#d9114a';
+            break;
+    
+        default:
+            break;
+    }
+  });
+  
+  document.addEventListener('keyup', function(event) {
+    switch (event.code) {
+        case "KeyA":
+            var key = document.getElementById('C' + octaveKeyboardGlobal);
+            key.style.fill = '#ffffff';
+            break;
+        case "KeyW":
+            var key = document.getElementById('C#' + octaveKeyboardGlobal);
+            key.style.fill = '#000000';
+            break;
+        case "KeyS":
+            var key = document.getElementById('D' + octaveKeyboardGlobal);
+            key.style.fill = '#ffffff';
+            break;
+        case "KeyE":
+            var key = document.getElementById('D#' + octaveKeyboardGlobal);
+            key.style.fill = '#000000';
+            break;
+        case "KeyD":
+            var key = document.getElementById('E' + octaveKeyboardGlobal);
+            key.style.fill = '#ffffff';
+            break;
+        case "KeyF":
+            var key = document.getElementById('F' + octaveKeyboardGlobal);
+            key.style.fill = '#ffffff';
+            break;
+        case "KeyT":
+            var key = document.getElementById('F#' + octaveKeyboardGlobal);
+            key.style.fill = '#000000';
+            break;
+        case "KeyG":
+            var key = document.getElementById('G' + octaveKeyboardGlobal);
+            key.style.fill = '#ffffff';
+            break;
+        case "KeyY":
+            var key = document.getElementById('G#' + octaveKeyboardGlobal);
+            key.style.fill = '#000000';
+            break;
+        case "KeyH":
+            var key = document.getElementById('A' + octaveKeyboardGlobal);
+            key.style.fill = '#ffffff';
+            break;
+        case "KeyU":
+            var key = document.getElementById('A#' + octaveKeyboardGlobal);
+            key.style.fill = '#000000';
+            break;
+        case "KeyJ":
+            var key = document.getElementById('B' + octaveKeyboardGlobal);
+            key.style.fill = '#ffffff';
+            break;
+    
+        default:
+            break;
+    }
+  });
+
+  function octShow(label){
+    document.getElementById(label).innerHTML = octaveKeyboardGlobal;
+  }
+
+  function octUp(){
+    octaveKeyboardGlobal += 1;
+    octShow("octaveLabel");
+  }
+
+  function octDown(){
+    octaveKeyboardGlobal -= 1;
+    octShow("octaveLabel");
+  }
---
+++ keysList.js
@@ -0,0 +1,618 @@
+var keysListGlobal = [
+    {
+        "octave": "",
+        "nameNotes": "A0",
+        "keyNumberPiano": "1",
+        "keyNumberOrgan": "",
+        "position": "0"
+    },
+    {
+        "octave": "",
+        "nameNotes": "A#0",
+        "keyNumberPiano": "2",
+        "keyNumberOrgan": "",
+        "position": "12"
+    },
+    {
+        "octave": "",
+        "nameNotes": "B0",
+        "keyNumberPiano": "3",
+        "keyNumberOrgan": "",
+        "position": "20"
+    },
+    {
+        "octave": "",
+        "nameNotes": "C1",
+        "keyNumberPiano": "4",
+        "keyNumberOrgan": "",
+        "position": "40"
+    },
+    {
+        "octave": "",
+        "nameNotes": "C#1",
+        "keyNumberPiano": "5",
+        "keyNumberOrgan": "",
+        "position": "52"
+    },
+    {
+        "octave": "",
+        "nameNotes": "D1",
+        "keyNumberPiano": "6",
+        "keyNumberOrgan": "",
+        "position": "60"
+    },
+    {
+        "octave": "",
+        "nameNotes": "D#1",
+        "keyNumberPiano": "7",
+        "keyNumberOrgan": "",
+        "position": "72"
+    },
+    {
+        "octave": "",
+        "nameNotes": "E1",
+        "keyNumberPiano": "8",
+        "keyNumberOrgan": "",
+        "position": "80"
+    },
+    {
+        "octave": "",
+        "nameNotes": "F1",
+        "keyNumberPiano": "9",
+        "keyNumberOrgan": "",
+        "position": "100"
+    },
+    {
+        "octave": "",
+        "nameNotes": "F#1",
+        "keyNumberPiano": "10",
+        "keyNumberOrgan": "",
+        "position": "112"
+    },
+    {
+        "octave": "",
+        "nameNotes": "G1",
+        "keyNumberPiano": "11",
+        "keyNumberOrgan": "",
+        "position": "120"
+    },
+    {
+        "octave": "",
+        "nameNotes": "G#1",
+        "keyNumberPiano": "12",
+        "keyNumberOrgan": "",
+        "position": "132"
+    },
+    {
+        "octave": "",
+        "nameNotes": "A1",
+        "keyNumberPiano": "13",
+        "keyNumberOrgan": "",
+        "position": "140"
+    },
+    {
+        "octave": "",
+        "nameNotes": "A#1",
+        "keyNumberPiano": "14",
+        "keyNumberOrgan": "",
+        "position": "152"
+    },
+    {
+        "octave": "",
+        "nameNotes": "B1",
+        "keyNumberPiano": "15",
+        "keyNumberOrgan": "",
+        "position": "160"
+    },
+    {
+        "octave": "",
+        "nameNotes": "C2",
+        "keyNumberPiano": "16",
+        "keyNumberOrgan": "1",
+        "position": "180"
+    },
+    {
+        "octave": "",
+        "nameNotes": "C#2",
+        "keyNumberPiano": "17",
+        "keyNumberOrgan": "2",
+        "position": "192"
+    },
+    {
+        "octave": "",
+        "nameNotes": "D2",
+        "keyNumberPiano": "18",
+        "keyNumberOrgan": "3",
+        "position": "200"
+    },
+    {
+        "octave": "",
+        "nameNotes": "D#2",
+        "keyNumberPiano": "19",
+        "keyNumberOrgan": "4",
+        "position": "212"
+    },
+    {
+        "octave": "",
+        "nameNotes": "E2",
+        "keyNumberPiano": "20",
+        "keyNumberOrgan": "5",
+        "position": "220"
+    },
+    {
+        "octave": "",
+        "nameNotes": "F2",
+        "keyNumberPiano": "21",
+        "keyNumberOrgan": "6",
+        "position": "240"
+    },
+    {
+        "octave": "",
+        "nameNotes": "F#2",
+        "keyNumberPiano": "22",
+        "keyNumberOrgan": "7",
+        "position": "252"
+    },
+    {
+        "octave": "",
+        "nameNotes": "G2",
+        "keyNumberPiano": "23",
+        "keyNumberOrgan": "8",
+        "position": "260"
+    },
+    {
+        "octave": "",
+        "nameNotes": "G#2",
+        "keyNumberPiano": "24",
+        "keyNumberOrgan": "9",
+        "position": "272"
+    },
+    {
+        "octave": "",
+        "nameNotes": "A2",
+        "keyNumberPiano": "25",
+        "keyNumberOrgan": "10",
+        "position": "280"
+    },
+    {
+        "octave": "",
+        "nameNotes": "A#2",
+        "keyNumberPiano": "26",
+        "keyNumberOrgan": "11",
+        "position": "292"
+    },
+    {
+        "octave": "",
+        "nameNotes": "B2",
+        "keyNumberPiano": "27",
+        "keyNumberOrgan": "12",
+        "position": "300"
+    },
+    {
+        "octave": "",
+        "nameNotes": "C3",
+        "keyNumberPiano": "28",
+        "keyNumberOrgan": "13",
+        "position": "320"
+    },
+    {
+        "octave": "",
+        "nameNotes": "C#3",
+        "keyNumberPiano": "29",
+        "keyNumberOrgan": "14",
+        "position": "332"
+    },
+    {
+        "octave": "",
+        "nameNotes": "D3",
+        "keyNumberPiano": "30",
+        "keyNumberOrgan": "15",
+        "position": "340"
+    },
+    {
+        "octave": "",
+        "nameNotes": "D#3",
+        "keyNumberPiano": "31",
+        "keyNumberOrgan": "16",
+        "position": "352"
+    },
+    {
+        "octave": "",
+        "nameNotes": "E3",
+        "keyNumberPiano": "32",
+        "keyNumberOrgan": "17",
+        "position": "360"
+    },
+    {
+        "octave": "",
+        "nameNotes": "F3",
+        "keyNumberPiano": "33",
+        "keyNumberOrgan": "18",
+        "position": "380"
+    },
+    {
+        "octave": "",
+        "nameNotes": "F#3",
+        "keyNumberPiano": "34",
+        "keyNumberOrgan": "19",
+        "position": "392"
+    },
+    {
+        "octave": "",
+        "nameNotes": "G3",
+        "keyNumberPiano": "35",
+        "keyNumberOrgan": "20",
+        "position": "400"
+    },
+    {
+        "octave": "",
+        "nameNotes": "G#3",
+        "keyNumberPiano": "36",
+        "keyNumberOrgan": "21",
+        "position": "412"
+    },
+    {
+        "octave": "",
+        "nameNotes": "A3",
+        "keyNumberPiano": "37",
+        "keyNumberOrgan": "22",
+        "position": "420"
+    },
+    {
+        "octave": "",
+        "nameNotes": "A#3",
+        "keyNumberPiano": "38",
+        "keyNumberOrgan": "23",
+        "position": "432"
+    },
+    {
+        "octave": "",
+        "nameNotes": "B3",
+        "keyNumberPiano": "39",
+        "keyNumberOrgan": "24",
+        "position": "440"
+    },
+    {
+        "octave": "",
+        "nameNotes": "C4",
+        "keyNumberPiano": "40",
+        "keyNumberOrgan": "25",
+        "position": "460"
+    },
+    {
+        "octave": "",
+        "nameNotes": "C#4",
+        "keyNumberPiano": "41",
+        "keyNumberOrgan": "26",
+        "position": "472"
+    },
+    {
+        "octave": "",
+        "nameNotes": "D4",
+        "keyNumberPiano": "42",
+        "keyNumberOrgan": "27",
+        "position": "480"
+    },
+    {
+        "octave": "",
+        "nameNotes": "D#4",
+        "keyNumberPiano": "43",
+        "keyNumberOrgan": "28",
+        "position": "492"
+    },
+    {
+        "octave": "",
+        "nameNotes": "E4",
+        "keyNumberPiano": "44",
+        "keyNumberOrgan": "29",
+        "position": "500"
+    },
+    {
+        "octave": "",
+        "nameNotes": "F4",
+        "keyNumberPiano": "45",
+        "keyNumberOrgan": "30",
+        "position": "520"
+    },
+    {
+        "octave": "",
+        "nameNotes": "F#4",
+        "keyNumberPiano": "46",
+        "keyNumberOrgan": "31",
+        "position": "532"
+    },
+    {
+        "octave": "",
+        "nameNotes": "G4",
+        "keyNumberPiano": "47",
+        "keyNumberOrgan": "32",
+        "position": "540"
+    },
+    {
+        "octave": "",
+        "nameNotes": "G#4",
+        "keyNumberPiano": "48",
+        "keyNumberOrgan": "33",
+        "position": "552"
+    },
+    {
+        "octave": "",
+        "nameNotes": "A4",
+        "keyNumberPiano": "49",
+        "keyNumberOrgan": "34",
+        "position": "560"
+    },
+    {
+        "octave": "",
+        "nameNotes": "A#4",
+        "keyNumberPiano": "50",
+        "keyNumberOrgan": "35",
+        "position": "572"
+    },
+    {
+        "octave": "",
+        "nameNotes": "B4",
+        "keyNumberPiano": "51",
+        "keyNumberOrgan": "36",
+        "position": "580"
+    },
+    {
+        "octave": "",
+        "nameNotes": "C5",
+        "keyNumberPiano": "52",
+        "keyNumberOrgan": "37",
+        "position": "600"
+    },
+    {
+        "octave": "",
+        "nameNotes": "C#5",
+        "keyNumberPiano": "53",
+        "keyNumberOrgan": "38",
+        "position": "612"
+    },
+    {
+        "octave": "",
+        "nameNotes": "D5",
+        "keyNumberPiano": "54",
+        "keyNumberOrgan": "39",
+        "position": "620"
+    },
+    {
+        "octave": "",
+        "nameNotes": "D#5",
+        "keyNumberPiano": "55",
+        "keyNumberOrgan": "40",
+        "position": "632"
+    },
+    {
+        "octave": "",
+        "nameNotes": "E5",
+        "keyNumberPiano": "56",
+        "keyNumberOrgan": "41",
+        "position": "640"
+    },
+    {
+        "octave": "",
+        "nameNotes": "F5",
+        "keyNumberPiano": "57",
+        "keyNumberOrgan": "42",
+        "position": "660"
+    },
+    {
+        "octave": "",
+        "nameNotes": "F#5",
+        "keyNumberPiano": "58",
+        "keyNumberOrgan": "43",
+        "position": "672"
+    },
+    {
+        "octave": "",
+        "nameNotes": "G5",
+        "keyNumberPiano": "59",
+        "keyNumberOrgan": "44",
+        "position": "680"
+    },
+    {
+        "octave": "",
+        "nameNotes": "G#5",
+        "keyNumberPiano": "60",
+        "keyNumberOrgan": "45",
+        "position": "692"
+    },
+    {
+        "octave": "",
+        "nameNotes": "A5",
+        "keyNumberPiano": "61",
+        "keyNumberOrgan": "46",
+        "position": "700"
+    },
+    {
+        "octave": "",
+        "nameNotes": "A#5",
+        "keyNumberPiano": "62",
+        "keyNumberOrgan": "47",
+        "position": "712"
+    },
+    {
+        "octave": "",
+        "nameNotes": "B5",
+        "keyNumberPiano": "63",
+        "keyNumberOrgan": "48",
+        "position": "720"
+    },
+    {
+        "octave": "",
+        "nameNotes": "C6",
+        "keyNumberPiano": "64",
+        "keyNumberOrgan": "49",
+        "position": "740"
+    },
+    {
+        "octave": "",
+        "nameNotes": "C#6",
+        "keyNumberPiano": "65",
+        "keyNumberOrgan": "50",
+        "position": "752"
+    },
+    {
+        "octave": "",
+        "nameNotes": "D6",
+        "keyNumberPiano": "66",
+        "keyNumberOrgan": "51",
+        "position": "760"
+    },
+    {
+        "octave": "",
+        "nameNotes": "D#6",
+        "keyNumberPiano": "67",
+        "keyNumberOrgan": "52",
+        "position": "772"
+    },
+    {
+        "octave": "",
+        "nameNotes": "E6",
+        "keyNumberPiano": "68",
+        "keyNumberOrgan": "53",
+        "position": "780"
+    },
+    {
+        "octave": "",
+        "nameNotes": "F6",
+        "keyNumberPiano": "69",
+        "keyNumberOrgan": "54",
+        "position": "800"
+    },
+    {
+        "octave": "",
+        "nameNotes": "F#6",
+        "keyNumberPiano": "70",
+        "keyNumberOrgan": "55",
+        "position": "812"
+    },
+    {
+        "octave": "",
+        "nameNotes": "G6",
+        "keyNumberPiano": "71",
+        "keyNumberOrgan": "56",
+        "position": "820"
+    },
+    {
+        "octave": "",
+        "nameNotes": "G#6",
+        "keyNumberPiano": "72",
+        "keyNumberOrgan": "57",
+        "position": "832"
+    },
+    {
+        "octave": "",
+        "nameNotes": "A6",
+        "keyNumberPiano": "73",
+        "keyNumberOrgan": "58",
+        "position": "840"
+    },
+    {
+        "octave": "",
+        "nameNotes": "A#6",
+        "keyNumberPiano": "74",
+        "keyNumberOrgan": "59",
+        "position": "852"
+    },
+    {
+        "octave": "",
+        "nameNotes": "B6",
+        "keyNumberPiano": "75",
+        "keyNumberOrgan": "60",
+        "position": "860"
+    },
+    {
+        "octave": "",
+        "nameNotes": "C7",
+        "keyNumberPiano": "76",
+        "keyNumberOrgan": "61",
+        "position": "880"
+    },
+    {
+        "octave": "",
+        "nameNotes": "C#7",
+        "keyNumberPiano": "77",
+        "keyNumberOrgan": "",
+        "position": "892"
+    },
+    {
+        "octave": "",
+        "nameNotes": "D7",
+        "keyNumberPiano": "78",
+        "keyNumberOrgan": "",
+        "position": "900"
+    },
+    {
+        "octave": "",
+        "nameNotes": "D#7",
+        "keyNumberPiano": "79",
+        "keyNumberOrgan": "",
+        "position": "912"
+    },
+    {
+        "octave": "",
+        "nameNotes": "E7",
+        "keyNumberPiano": "80",
+        "keyNumberOrgan": "",
+        "position": "920"
+    },
+    {
+        "octave": "",
+        "nameNotes": "F7",
+        "keyNumberPiano": "81",
+        "keyNumberOrgan": "",
+        "position": "940"
+    },
+    {
+        "octave": "",
+        "nameNotes": "F#7",
+        "keyNumberPiano": "82",
+        "keyNumberOrgan": "",
+        "position": "952"
+    },
+    {
+        "octave": "",
+        "nameNotes": "G7",
+        "keyNumberPiano": "83",
+        "keyNumberOrgan": "",
+        "position": "960"
+    },
+    {
+        "octave": "",
+        "nameNotes": "G#7",
+        "keyNumberPiano": "84",
+        "keyNumberOrgan": "",
+        "position": "972"
+    },
+    {
+        "octave": "",
+        "nameNotes": "A7",
+        "keyNumberPiano": "85",
+        "keyNumberOrgan": "",
+        "position": "980"
+    },
+    {
+        "octave": "",
+        "nameNotes": "A#7",
+        "keyNumberPiano": "86",
+        "keyNumberOrgan": "",
+        "position": "992"
+    },
+    {
+        "octave": "",
+        "nameNotes": "B7",
+        "keyNumberPiano": "87",
+        "keyNumberOrgan": "",
+        "position": "1000"
+    },
+    {
+        "octave": "",
+        "nameNotes": "C8",
+        "keyNumberPiano": "88",
+        "keyNumberOrgan": "",
+        "position": "1020"
+    }
+]
